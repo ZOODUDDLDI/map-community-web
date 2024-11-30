6b6d45380c7f820a613f383da39d1564efc1c172
ZOODUDDLDI <wndud238561@gmail.com>
2024-11-30 15:24:59 +0900

    [update] layout 요소 추가, 디자인 변경.

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,10 +1,25 @@
 <template>
-  <div>
-    <div class="text-h2">Default layouts</div>
-    <router-view></router-view>
-  </div>
+  <q-layout view="hHh lpR fFf">
+    <q-header bordered class="bg-white text-grey-9">
+      <q-toolbar>
+        <q-btn flat dense to="/">
+          <q-toolbar-title>
+            <q-avatar>
+              <img src="/logo.png" />
+            </q-avatar>
+            MapTalk
+          </q-toolbar-title>
+        </q-btn>
+        <q-space />
+        <q-btn stretch flat label="Home" to="/" />
+        <q-btn stretch flat label="MyPage" to="/mypage" />
+        <q-separator class="q-my-md q-mr-md" vetical />
+        <q-btn unelevate rounded color="orange" label="로그인 / 회원가입" />
+      </q-toolbar>
+    </q-header>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
 </template>
-
-<script setup></script>
-
-<style lang="scss" scoped></style>
