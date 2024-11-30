407268f1ad9bb6b9198fbf8a6e24af3efbe34f72
ZOODUDDLDI <wndud238561@gmail.com>
2024-11-30 14:04:43 +0900

    [add] 페이지 추가(목록,상세,수정/마이페이지,프로필,패스워드,북마크)

--- src/pages/posts/[id]/index.vue
+++ src/pages/posts/[id]/index.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="text-4">커뮤니티 목록</div>
+  <div class="text-4">게시글 상세</div>
   <ul>
     <li>
-      <router-link to="/posts/1">1번 게시글</router-link>
+      <router-link to="/posts/1/edit">수정하기</router-link>
     </li>
   </ul>
 </template>
