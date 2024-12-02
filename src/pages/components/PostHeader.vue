1fcffa5e5096420f172af1f268da0bd6ca6e4bc9
ZOODUDDLDI <wndud238561@gmail.com>
2024-12-02 10:13:00 +0900

    [update] 커뮤니티 헤더 ui 배치

--- src/pages/components/PostHeader.vue
+++ src/pages/components/PostHeader.vue
@@ -1,6 +1,13 @@
 <template>
-  <div>
+  <div class="flex items-center q-mb-lg">
     <div class="text-5">커뮤니티 목록</div>
+    <q-space />
+    <q-tabs narrow-indicator dense>
+      <q-tab :rapple="false" label="위치순" name="location" />
+      <q-tab :rapple="false" label="최신순" name="createTime" />
+      <q-tab :rapple="false" label="조회순" name="readCount" />
+      <q-tab :rapple="false" label="좋아요순" name="likeCount" />
+    </q-tabs>
   </div>
 </template>
 
