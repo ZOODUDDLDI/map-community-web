6689831882304fcb6ba686ae83dd5f9ec2643920
ZOODUDDLDI <wndud238561@gmail.com>
2024-12-02 13:52:00 +0900

    [update] 게시글 상세, 댓글 컴포넌트 분리

---
+++ src/pages/posts/[id]/components/PostComments.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <div class="text-subtitle text-weight-bold q-mb-lg">댓글 6</div>
+
+    <div v-if="isActive">
+      <q-input type="textarea" class="bg-whithe" outlined></q-input>
+      <div class="flex justify-end q-gutter-x-sm q-mt-sm">
+        <q-btn label="취소" color="dark" unelevated @click="toggleActive" />
+        <q-btn label="등록" color="primary" unelevated />
+      </div>
+    </div>
+
+    <q-card v-if="!isActive" @click="toggleActive" class="cursor-pointer">
+      <q-card-section class="flex items-center">
+        <q-avatar>
+          <img src="/juyeong.png" alt="" />
+        </q-avatar>
+        <div class="text-grey-6 q-ml-md">댓글을 작성해보세요.</div>
+      </q-card-section>
+    </q-card>
+
+    <q-list class="q-mt-lg bg-whithe" bordered separator>
+      <q-item>
+        <q-item-section side top>
+          <q-avatar size="md">
+            <img src="/juyeong.png" alt="" />
+          </q-avatar>
+        </q-item-section>
+        <q-item-section>
+          <div class="flex text-caption">
+            <span>쥉</span>
+            <span class="q-mx-xs">&middot;</span>
+            <span class="text-grey-6">1일전</span>
+          </div>
+          <div class="q-mt-sm">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorem
+            vero adipisci placeat suscipit quis voluptate! Totam alias neque
+            repellendus eligendi aperiam, provident quos necessitatibus quia
+            dolorum quisquam fugiat ea magnam.
+          </div>
+        </q-item-section>
+        <q-item-section side top>
+          <q-btn flat color="grey" icon="sym_o_delete" round dense></q-btn>
+        </q-item-section>
+      </q-item>
+    </q-list>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const isActive = ref(false);
+const toggleActive = () => (isActive.value = !isActive.value);
+</script>
+
+<style lang="scss" scoped></style>
--- src/pages/posts/[id]/index.vue
+++ src/pages/posts/[id]/index.vue
@@ -1,81 +1,15 @@
 <template>
   <q-page padding>
-    <q-card class="q-pa-lg">
-      <div class="flex q-mb-md">
-        <q-btn
-          icon="sym_o_arrow_back"
-          flat
-          round
-          dense
-          color="grey"
-          size="16px"
-        />
-        <q-space />
-        <q-btn icon="sym_o_favorite" flat round dense color="red" size="16px" />
-        <q-btn
-          icon="sym_o_bookmark"
-          flat
-          round
-          dense
-          color="blue"
-          size="16px"
-        />
-      </div>
-      <div class="flex items-center">
-        <q-avatar>
-          <img src="/juyeong.png" alt="" />
-        </q-avatar>
-        <div class="q-ml-md">
-          <div>쥉</div>
-          <div class="text-grey-6">1일전</div>
-        </div>
-        <q-space />
-        <q-btn icon="more_horiz" round flat>
-          <q-menu>
-            <q-list style="min-width: 100px">
-              <q-item clickable v-close-popup>
-                <q-item-section>수정하기</q-item-section>
-              </q-item>
-              <q-item clickable v-close-popup>
-                <q-item-section>삭제하기</q-item-section>
-              </q-item>
-            </q-list>
-          </q-menu>
-        </q-btn>
-      </div>
+    <PostDetails />
+    <q-separator class="q-my-xl" />
 
-      <div class="q-mt-md text-5 text-weight-bold">제목입니다</div>
-
-      <div class="row items-center q-gutter-x-md q-mt-md justify-end">
-        <PostIcon name="sym_o_visibility" :label="1" tooltip="조회수" />
-
-        <PostIcon name="sym_o_sms" :label="2" tooltip="댓글수" />
-
-        <PostIcon name="sym_o_favorite" :label="3" tooltip="좋아요" />
-
-        <PostIcon name="sym_o_bookmark" :label="4" tooltip="북마크" />
-      </div>
-      <q-separator class="q-my-lg" />
-      <div>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet magnam
-        cumque velit minus ad voluptas accusamus reprehenderit numquam error
-        vero id omnis, voluptatibus ratione dolorem cum ipsum eius repudiandae
-        accusantium! <br />
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet magnam
-        cumque velit minus ad voluptas accusamus reprehenderit numquam error
-        vero id omnis, voluptatibus ratione dolorem cum ipsum eius repudiandae
-        accusantium! <br />
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet magnam
-        cumque velit minus ad voluptas accusamus reprehenderit numquam error
-        vero id omnis, voluptatibus ratione dolorem cum ipsum eius repudiandae
-        accusantium!
-      </div>
-    </q-card>
+    <PostComments />
   </q-page>
 </template>
 
 <script setup>
-import PostIcon from 'src/components/apps/post/PostIcon.vue';
+import PostDetails from './components/PostDetails.vue';
+import PostComments from './components/PostComments.vue';
 </script>
 
 <style lang="scss" scoped></style>
