c1a28f7cee5497c846d76381acd555fcfe33f5cc
ZOODUDDLDI <wndud238561@gmail.com>
2024-12-03 09:52:02 +0900

    [update] 파이어베이스 설치, sdk 추가

--- quasar.config.js
+++ quasar.config.js
@@ -18,7 +18,7 @@ module.exports = configure(function (/* ctx */) {
     // app boot file (/src/boot)
     // --> boot files are part of "main.js"
     // https://v2.quasar.dev/quasar-cli-vite/boot-files
-    boot: ['axios'],
+    boot: ['axios', 'firebase'], //TODO: 파이어 베이스 등록.
 
     // https://v2.quasar.dev/quasar-cli-vite/quasar-config-js#css
     css: ['app.scss'],
