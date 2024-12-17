278b06335a7962e2e49285903b80cd3e2da410c9
ZOODUDDLDI <wndud238561@gmail.com>
2024-12-17 11:28:48 +0900

    [update] 컴포넌트 크기 조정, transition none 설정.

--- src/components/apps/post/PostWriteDialog.vue
+++ src/components/apps/post/PostWriteDialog.vue
@@ -1,6 +1,12 @@
 <template>
-  <q-dialog persistent v-bind="$attrs" @hide="onHide">
-    <q-card :style="{ width: '660px' }">
+  <q-dialog
+    persistent
+    v-bind="$attrs"
+    @hide="onHide"
+    transition-show="none"
+    transition-hide="none"
+  >
+    <q-card :style="{ minWidth: '660px' }">
       <q-toolbar>
         <q-toolbar-title>글쓰기</q-toolbar-title>
         <q-btn v-close-popup flat round dense icon="close" />
@@ -26,7 +32,6 @@ const getInitialForm = () => ({
 
 <script setup>
 import { ref } from 'vue';
-import { getCategories } from 'src/services/category';
 import PostForm from './PostForm.vue';
 
 // 폼
@@ -35,7 +40,6 @@ const form = ref(getInitialForm());
 // 다이얼로그 닫히면 초기화 시키기
 const onHide = () => {
   form.value = getInitialForm();
-  tagField.value = '';
 };
 </script>
 
