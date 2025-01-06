e7bad7badd8c3f96469891548bd82055597aad04
ZOODUDDLDI <wndud238561@gmail.com>
2025-01-06 12:59:26 +0900

    [update] 인라인컴포저블로 사용하기.

--- src/components/apps/post/PostItem.vue
+++ src/components/apps/post/PostItem.vue
@@ -83,36 +83,47 @@ const props = defineProps({
 const { uid, isAuthenticated } = storeToRefs(useAuthStore());
 const { id: postId, likeCount: initialCount } = toRefs(props.item);
 
-// 좋아요 기능
-const isLike = ref(false);
-const likeCount = ref(initialCount.value);
+// 인라인 컴포저블
+const useLike = () => {
+  // 좋아요 기능
+  const isLike = ref(false);
+  const likeCount = ref(initialCount.value);
 
-// 조회
-const initLike = async () => {
-  if (isAuthenticated.value === false) {
-    isLike.value = false;
-    return;
-  }
-  isLike.value = await hasLike(uid.value, postId.value);
+  // 조회
+  const initLike = async () => {
+    if (isAuthenticated.value === false) {
+      isLike.value = false;
+      return;
+    }
+    isLike.value = await hasLike(uid.value, postId.value);
+  };
+
+  const toggleLike = async () => {
+    if (isAuthenticated.value === false) {
+      alert('로그인 후 이용 가능합니다.');
+      return;
+    }
+    if (isLike.value) {
+      await removeLike(uid.value, postId.value);
+      likeCount.value--;
+    } else {
+      await addLike(uid.value, postId.value);
+      likeCount.value++;
+    }
+    isLike.value = !isLike.value;
+  };
+
+  // 로그인 상태 감지
+  watch(isAuthenticated, () => initLike(), { immediate: true });
+
+  return {
+    isLike,
+    likeCount,
+    toggleLike,
+  };
 };
 
-const toggleLike = async () => {
-  if (isAuthenticated.value === false) {
-    alert('로그인 후 이용 가능합니다.');
-    return;
-  }
-  if (isLike.value) {
-    await removeLike(uid.value, postId.value);
-    likeCount.value--;
-  } else {
-    await addLike(uid.value, postId.value);
-    likeCount.value++;
-  }
-  isLike.value = !isLike.value;
-};
-
-// 로그인 상태 감지
-watch(isAuthenticated, () => initLike(), { immediate: true });
+const { isLike, likeCount, toggleLike } = useLike();
 </script>
 
 <style lang="scss" scoped></style>
